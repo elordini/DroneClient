67bc45c1eadbf246681cca4eae8abf48cef634e8
elordini <elordini@gmail.com>
2012-05-16 16:53:38 -0600

    comments on future possibilities

--- DroneClient/Main.cs
+++ DroneClient/Main.cs
@@ -11,6 +11,18 @@ namespace DroneClient
 			MainWindow win = new MainWindow ();
 			win.Show ();
 			Application.Run ();
+			// If the chat server uses an irc protocol, here's a potential
+			// client we can pull from: http://code.google.com/p/monc/
+			//
+			
+			//Here's a generic list of things we should probably accomplish
+			//
+			// 1. check to see if we have login credentials stored on the disk
+			//		1a. if so, 
+			//			1a1. login
+			//			1a2. download current master task list
+			//			1a3. generate today's task list
+			//
 		}
 	}
 }
